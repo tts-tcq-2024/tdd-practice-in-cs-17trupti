1563bd218a57ffe2cd9780e2f23644716b3375cc
17trupti <167959684+17trupti@users.noreply.github.com>
2024-09-19 16:19:11 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -7,16 +7,34 @@ public class StringCalculator
             return 0;
         }
 
+        return CalculateSum(numbers);
+    }
+
+    private int CalculateSum(string numbers)
+    {
         string[] numArray = numbers.Split(',');
         int sum = 0;
+        List<int> negativeNumbers = new List<int>();
 
         foreach (string num in numArray)
         {
-            sum += int.Parse(num);
+            int parsedNum = int.Parse(num);
+            if (parsedNum < 0)
+            {
+                negativeNumbers.Add(parsedNum);
+            }
+            else if (parsedNum <= 1000)
+            {
+                sum += parsedNum;
+            }
+        }
+
+        if (negativeNumbers.Count > 0)
+        {
+            throw new Exception($"Negatives not allowed: {string.Join(",", negativeNumbers)}");
         }
 
         return sum;
     }
 }
-
 
