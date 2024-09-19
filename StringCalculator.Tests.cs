df74ea406910f2230a336c9bba31a88963012809
17trupti <167959684+17trupti@users.noreply.github.com>
2024-09-19 17:34:39 +0530

    Update StringCalculator.Tests.cs

--- StringCalculator.Tests.cs
+++ StringCalculator.Tests.cs
@@ -91,39 +91,6 @@ public class StringCalculatorAddTests
 
         Assert.Equal(expectedResult, result);
     }
-    /*
-    [Fact]
-    public void Add_ShouldReturnSumWithMultipleCharacterCustomDelimiter_WhenInputHasMultipleCharacterDelimiter()
-    {
-        int expectedResult = 6;
-        string input = "//[***]\n1***2***3";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.Add(input);
-        
-        Assert.Equal(expectedResult, result);
-    }
-    
-    [Fact]
-    public void Add_ShouldReturnSumWithMixedDelimiters_WhenInputHasMixedDelimiters()
-    {
-        int expectedResult = 6;
-        string input = "//;\n1;2\n3";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.Add(input);
-
-        Assert.Equal(expectedResult, result);
-    }
-    */
-    [Fact]
-    public void Add_ShouldReturnSumWithMultipleCustomDelimiters_WhenInputHasMultipleCustomDelimiters()
-    {
-        int expectedResult = 6;
-        string input = "//[;][%]\n1;2%3";
-        StringCalculator objUnderTest = new StringCalculator();
-        int result = objUnderTest.Add(input);
-
-        Assert.Equal(expectedResult, result);
-    }
     
     [Fact]
     public void Add_ShouldThrowExceptionForNegativeNumbersWithCustomDelimiters_WhenInputContainsNegativeNumbersWithCustomDelimiter()
