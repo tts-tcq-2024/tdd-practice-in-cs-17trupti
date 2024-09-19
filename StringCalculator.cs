f31fdcb2f7066d39ca861a628389e1665b097440
17trupti <167959684+17trupti@users.noreply.github.com>
2024-09-19 16:33:58 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 public class StringCalculator
 {
     public int Add(string numbers)
@@ -12,7 +16,7 @@ public class StringCalculator
 
     private int CalculateSum(string numbers)
     {
-        string[] numArray = numbers.Split(',');
+        string[] numArray = numbers.Split(new[] { ',', '\n' }, StringSplitOptions.None);
         List<int> negativeNumbers = new List<int>();
 
         int sum = numArray
