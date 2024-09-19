ae41bb3884165aba02047bdb0d634a3abd4913b7
17trupti <167959684+17trupti@users.noreply.github.com>
2024-09-19 17:35:48 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 public class StringCalculator
 {
@@ -19,34 +18,22 @@ public class StringCalculator
     {
         if (numbers.StartsWith("//"))
         {
-            return GetNumbersWithCustomDelimiters(numbers);
+            return GetNumbersWithCustomDelimiter(numbers);
         }
         
+        // Split input by default delimiters (comma and newline)
         return numbers.Split(new[] { ',', '\n' }, StringSplitOptions.None);
     }
 
-    private IEnumerable<string> GetNumbersWithCustomDelimiters(string numbers)
+    private IEnumerable<string> GetNumbersWithCustomDelimiter(string numbers)
     {
-        // Extract custom delimiters
+        // Extract custom delimiter
         int delimiterEndIndex = numbers.IndexOf('\n');
-        string delimiterPart = numbers.Substring(2, delimiterEndIndex - 2);
-        
-        // Extract delimiters using regex
-        var delimiters = new List<string> { ",", "\n" }; // Default delimiters
-        var matches = Regex.Matches(delimiterPart, @"\[(.*?)\]");
-        foreach (Match match in matches)
-        {
-            delimiters.Add(Regex.Escape(match.Groups[1].Value));
-        }
-        
-        // Extract numbers part
+        string delimiter = numbers.Substring(2, delimiterEndIndex - 2);
         string numberPart = numbers.Substring(delimiterEndIndex + 1);
-
-        // Join delimiters for regex pattern
-        string delimiterPattern = string.Join("|", delimiters);
         
-        // Split input by custom delimiters
-        return numberPart.Split(new[] { delimiterPattern }, StringSplitOptions.None);
+        // Split input by custom delimiter
+        return numberPart.Split(new[] { delimiter }, StringSplitOptions.None);
     }
 
     private int CalculateSum(IEnumerable<string> numbers)
